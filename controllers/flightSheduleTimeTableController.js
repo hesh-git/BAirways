c424ea661d9b6a6179d649ae4603c06527860c37
hasiniV19 <83814896+hasiniV19@users.noreply.github.com>
2022-05-23 00:04:36 +0530

    error in layout name of flight schedule corrected

--- controllers/flightSheduleTimeTableController.js
+++ controllers/flightSheduleTimeTableController.js
@@ -1,5 +1,5 @@
 const flightTimeTable_get = (req, res) => {
-    res.render('flightSheduleTimeTable', {title : 'Flight Shedules', layout: '.layouts/schedule_layout'});
+    res.render('flightSheduleTimeTable', {title : 'Flight Shedules', layout: './layouts/schedule_layout'});
 }
 
 module.exports = {
