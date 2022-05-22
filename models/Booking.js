2a0aafc97881485225c59b6420b251fad0273643
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2022-05-22 16:11:06 +0530

    passenger details added

--- models/Booking.js
+++ models/Booking.js
@@ -4,6 +4,20 @@ const save = (data, dbCon, callback) => {
     dbCon.query(sql, [1, 1, 1, data.Gender, data.FirstName, data.LastName, data.DateOfBirth], callback);
 }
 
+const addBooking = (FlightScheduleID, TravellerID, TravelClassID, BookingStateID, dbCon, callback) =>{
+    var sql_booking = 'INSERT INTO `Booking` (`FlightScheduleID`, `TravellerID`,`TravelClassID`, `BookingStateID`) VALUES (?,?,?,?)';
+    dbCon.query(sql_booking, [FlightScheduleID,TravellerID,TravelClassID,BookingStateID], callback);
+}
+
+const addPassenger = ( BookingID, TypeID, Gender, FirstName, LastName, DateOfBirth, dbCon, callback) =>{
+    
+    var sql_passenger = 'INSERT INTO `passengerdetails` (`BookingID`, `TypeID`, `Gender`, `FirstName`, `LastName`, `DateOfBirth`) VALUES (?,?,?,?,?,?)';
+    dbCon.query(sql_passenger, [BookingID, TypeID, Gender, FirstName, LastName, DateOfBirth], callback);
+    
+}
+
 module.exports = {
-    save
+    save,
+    addPassenger,
+    addBooking
 }
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,4 +6,7 @@ const router = express.Router();
 router.get('/passengerDetails', BookingController.add_passenger_details_get);
 router.post('/passengerDetails', BookingController.add_passenger_details_post);
 
+router.get('/guestDetails', BookingController.add_guest_details_get);
+// router.post('/guestDetails', BookingController.add_guest_details_post);
+
 module.exports = router;
