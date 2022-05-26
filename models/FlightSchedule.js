bcbf5bbd3bc233d19568af5c4d7018a40cf8dbc0
hasiniV19 <83814896+hasiniV19@users.noreply.github.com>
2022-05-26 23:26:03 +0530

    in flight schedule, datetime broken into 2 col

--- models/FlightSchedule.js
+++ models/FlightSchedule.js
@@ -1,8 +1,8 @@
-const add_flight_schedule = (FlightNo, AircraftID, StateID, StartTime, EndTime, dbCon, callback) => {
+const add_flight_schedule = (FlightNo, AircraftID, StateID, DepartureDate, DepartureTime, ArrivalDate, ArrivalTime, dbCon, callback) => {
 
-    const sql = "INSERT INTO `FlightSchedule`(`FlightNo`, `AircraftID`, `StateID`, `StartTime`, `EndTime`) VALUES (?,?,?,?,?)";
+    const sql = "INSERT INTO `FlightSchedule`(`FlightNo`, `AircraftID`, `StateID`, `DepartureDate`, `DepartureTime`, `ArrivalDate`, `ArrivalTime`) VALUES (?,?,?,?,?,?,?)";
 
-    dbCon.query(sql, [FlightNo, AircraftID, StateID, StartTime, EndTime], callback);
+    dbCon.query(sql, [FlightNo, AircraftID, StateID, DepartureDate, DepartureTime, ArrivalDate, ArrivalTime], callback);
 }
 
 module.exports = {
