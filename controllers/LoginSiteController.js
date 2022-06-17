cf28fda23d72b57753317a846d33ad33d0edb4a2
hesh-git <theshan.19@cse.mrt.ac.lk>
2022-06-17 11:41:39 +0530

    login signup validations

--- controllers/LoginSiteController.js
+++ controllers/LoginSiteController.js
@@ -1,5 +1,17 @@
 const userModel = require('../models/userModel');
 const authModel = require('../models/autheticationModel');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require("express-validator");
+
+
+const maxValidity = 3*24*60*60; //validity period of jwt
+
+const createToken = (id,userType) => {
+    return jwt.sign({id, userType}, 'B airways secret',{
+        expiresIn: maxValidity
+    });
+}
+
 
 const login_page = (req,res) => {
     res.render('login', {title: 'Login Page', layout: './layouts/auth_layout'})
@@ -13,31 +25,43 @@ const login_post = (req,res,next) => {
     const con = req.dbCon;
     var email = req.body.email;
     var password =req.body.password;
-    
-    userModel.getUserByEmail(email,con, function(err,result, fields){
-        console.log(result);
-        if(err) throw err;
-        if(!result.length){
-          res.send('Not logged in');
+
+    const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            // return res.status(422).jsonp(errors.array())
+            const alert = errors.array()[0]
+            console.log(alert)
+            res.render('login', {
+                 title: 'Login Page', layout: './layouts/auth_layout',alert
+            })
         }
-  
-        var id = result[0].ID;
-        
-        authModel.getPwrdByID(id, con, function(err,result, fields){
-            if(err) throw err;
-            //console.log(result);
-            if(result.length && result[0].password == password){
-                authModel.updateLastLog(id,con, function(err,result,fields){
-                    if (err) throw err;   
-                    req.session.email = email;
-                    res.send('Logged');
-                })
-            }else{
-            req.session.flag = 4;
-            res.send('Not logged');
-            }
-        });
-      });
+        else{
+            userModel.getUserByEmail(email,con, function(err,result, fields){
+                console.log(result);
+                if(err) throw err;
+                if(!result.length){
+                  res.send('Not logged in');
+                }
+                var id = result[0].ID;
+                
+                authModel.getPwrdByID(id, con, function(err,result, fields){
+                    if(err) throw err;
+                    //console.log(result);
+                    if(result.length && result[0].password == password){
+                        authModel.updateLastLog(id,con, function(err,result,fields){
+                            if (err) throw err;   
+                            req.session.email = email;
+                            res.send('Logged');
+                        })
+                    }else{
+                    req.session.flag = 4;
+                    res.send('Not logged');
+                    }
+                });
+              });
+        }
+    
+
   }
 
   const signup_post = (req,res,next) => {
@@ -51,18 +75,27 @@ const login_post = (req,res,next) => {
         const conPassword = data.conPassword;
         const contact = data.contact;
 
-        
-
-        //userModel.getUserByEmail(email,dbCon,function(err,result, fields){
-            //if(result == []){
-                userModel.addUser(fName,lName,email,password,contact,dbCon,function(err,result, fields){
-                    console.log(result);
-                    if(err)
-                        throw err
-                    res.send('User entered');
-                })
-            //}
-        //})
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            // return res.status(422).jsonp(errors.array())
+            const alert = errors.array()[0]
+            console.log(alert)
+            res.render('signup', {
+                 title: 'Signup Page', layout: './layouts/auth_layout',alert
+            })
+        }
+        else{        
+            userModel.addUser(fName,lName,email,password,contact,dbCon,function(err,result, fields){
+            if(err)
+                throw err
+            
+            const token = createToken(result.insertId, 'traveller'); 
+            res.cookie('jwt', token, {httpOnly: true, maxValidity: maxValidity*1000});
+            //res.send('User entered');
+            //res.status(201).json({user: result.insertId});
+            //console.log(result)   
+            
+        })}
 
   }
 
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,17 +1,53 @@
-const express = require('express');
-const loginSiteController = require('../controllers/LoginSiteController');
-
+const { urlencoded } = require("body-parser");
+const { check, validationResult } = require("express-validator");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const express = require("express");
+const loginSiteController = require("../controllers/LoginSiteController");
 
 const router = express.Router();
 
 //GET requests
-router.get('/', loginSiteController.login_page);
+router.get("/", loginSiteController.login_page);
 
-router.get('/register', loginSiteController.signup_page);
-router.post('/register',loginSiteController.signup_post);
+router.get("/register", loginSiteController.signup_page);
 
 //POST request
-router.post('/auth_login', loginSiteController.login_post);
-
+router.post(
+  "/auth_login",
+  urlencodedParser,
+  [
+    check("email", "Email is not valid").isEmail().normalizeEmail(),
+    check("password","Password should be filled")
+      .not()
+      .isEmpty()
+  ],
+  loginSiteController.login_post
+);
+router.post(
+  "/register",
+  urlencodedParser,
+  [
+    check("fName", "First name must be at least 3 characters long")
+      .exists()
+      .isLength({ min: 3, max: 30 }),
+    check("lName", "Last name must be at least 3 characters long")
+      .exists()
+      .isLength({ min: 3, max: 30 }),
+    check("email", "Email is not valid").isEmail().normalizeEmail(),
+    check("password","Password is not in the required form. Password should contain at least 8 characters and should be alphanumeric")
+      .not()
+      .isEmpty()
+      .isLength({ min: 8, max: 10 }),
+    check("conPassword", "Passwords do not match").custom(
+      (value, { req }) => value === req.body.password
+    ),
+    check("mobile", "Mobile number should contains 10 digits")
+      .isInt()
+      .exists()
+      .isLength({ min: 10, max: 10 }),
+  ],
+  loginSiteController.signup_post
+);
 
 module.exports = router;
