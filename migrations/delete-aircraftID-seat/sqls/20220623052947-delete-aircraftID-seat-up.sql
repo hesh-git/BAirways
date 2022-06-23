8c3f34577baea23878738fc0b78545c128ce99c7
hesh-git <theshan.19@cse.mrt.ac.lk>
2022-06-23 11:31:23 +0530

    delete aircraftID seat- changed

--- migrations/delete-aircraftID-seat/sqls/20220623052947-delete-aircraftID-seat-up.sql
+++ migrations/delete-aircraftID-seat/sqls/20220623052947-delete-aircraftID-seat-up.sql
@@ -1,3 +1,12 @@
+-- ALTER TABLE `airlinedbms`.`seat` 
+-- DROP COLUMN `AircraftID`,
+-- DROP PRIMARY KEY,
+-- ADD PRIMARY KEY (`SeatNo`),
+-- DROP INDEX `AircraftID` ;
+-- ;
+
+ALTER TABLE `airlinedbms`.`seat` 
+DROP FOREIGN KEY `seat_ibfk_2`;
 ALTER TABLE `airlinedbms`.`seat` 
 DROP COLUMN `AircraftID`,
 DROP PRIMARY KEY,
